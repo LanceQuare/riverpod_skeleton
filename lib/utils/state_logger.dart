32cbe3b68e256a3ee60115b7208345444ad655ca
Lance Johansson <quare23@gmail.com>
2024-06-03 12:17:35 +0800

    log state and error handling.

--- lib/utils/state_logger.dart
+++ lib/utils/state_logger.dart
@@ -5,6 +5,19 @@ import 'dart:developer' as developer;
 /// Read the logs and you'll better understand what's going on under the hood
 class StateLogger extends ProviderObserver {
   const StateLogger();
+
+  @override
+  void didAddProvider(ProviderBase<Object?> provider, Object? value,
+      ProviderContainer container) {
+    developer.log('Provider $provider was initialized with $value');
+  }
+
+  @override
+  void didDisposeProvider(
+      ProviderBase<Object?> provider, ProviderContainer container) {
+    developer.log('Provider $provider was disposed');
+  }
+
   @override
   void didUpdateProvider(
       ProviderBase provider,
@@ -13,11 +26,20 @@ class StateLogger extends ProviderObserver {
       ProviderContainer container,
       ) {
     developer.log('''
-{
-  provider: ${provider.name ?? provider.runtimeType},
-  oldValue: $previousValue,
-  newValue: $newValue
-}
-''');
+      {
+        provider: ${provider.name ?? provider.runtimeType},
+        oldValue: $previousValue,
+        newValue: $newValue
+      }
+    ''');
+  }
+
+  @override
+  void providerDidFail(
+      ProviderBase<Object?> provider,
+      Object error,
+      StackTrace stackTrace,
+      ProviderContainer container) {
+    developer.log('Provider $provider threw $error at $stackTrace');
   }
 }
